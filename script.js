a4f890b694d9d9737ed96b9e3b2b8a07e78291d8
brian120488 <brian.yang@csedge.org>
2019-02-16 18:12:49 -0500

    Updated Challenges

--- script.js
+++ script.js
@@ -1,32 +1,42 @@
+//Challenge Zero
 let button = document.getElementById("play-button");
 button.addEventListener("click", ifButtonPressed);
-button.addEventListener("mouseover", hover);
-let title = document.getElementById("page-title");
-title.addEventListener("copy", ifCopied);
-
-
 
 function ifButtonPressed() {
 	button.style.top = Math.random() * 100 + "%";
 	button.style.left = Math.random() * 100 + "%";
 }
+
+
+//Challenge One
+let text = document.getElementById("challenge-one");
+text.addEventListener("dblclick", ifClicked);
+
+function ifClicked() {
+	let body = document.getElementsByTagName("body")[0];
+	body.removeChild(text);
+}
+//Challenge Two
+button.addEventListener("mouseover", hover);
+
 function hover() {
 	button.style.backgroundColor = "blue";
 }
 
+//Challenge Three
+let title = document.getElementById("page-title");
+title.addEventListener("copy", ifCopied);
 
 function ifCopied() {
-	let alert = document.createElement("h1");
-	alert.innerHTML = "DON'T COPY THIS";
-	title.appendChild(alert);
+	alert("DON'T COPY THIS");
+}
+
+//Challenge Four
+let body = document.getElementsByTagName("body")[0];
+body.addEventListener("contextmenu", ifRightClicked);
+
+function ifRightClicked() {
+	body.style.backgroundImage = "url('images/pokemonBG2.png')";
 }
 
 
-let text = document.getElementById("challenge-one");
-text.addEventListener("dblclick", ifTextPressed);
-
-function ifTextPressed() {
-	let html = document.getElementsByTagName("HMTL");
-	let body = html[1]
-	body.removeChild(text);
-}
